8d10447944e9eb212d3ad3d8dce8809dc3c75f9e
Stephanie Zou <stephanie.zou@stern.nyu.edu>
2020-03-02 15:05:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,7 +20,7 @@ def load_library(file_path)
 
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file_path, emoticon)
   result = load_library(file_path)['get_emoticon'][emoticon]
   result ? result : "Sorry, that emoticon was not found"
 
