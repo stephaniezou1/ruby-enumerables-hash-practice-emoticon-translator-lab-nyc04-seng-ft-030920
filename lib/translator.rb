54bd110d2c914f6637771cc127f931117e99bbf8
Stephanie Zou <stephanie.zou@stern.nyu.edu>
2020-03-02 15:04:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,6 +2,7 @@
 require "yaml"
 
 def load_library(file_path)
+  
   emoticons = YAML.load_file(file_path)
   
   new_hash = {
@@ -9,7 +10,14 @@ def load_library(file_path)
     'get_emoticon' => {}
   }
   
+  emoticons.each do |key,value|
+    new_hash['get_emoticon'][value[0]] = emoticons[key][1]
+    new_hash['get_meaning'][value[1]] = key
   
+  end
+
+  new_hash
+
 end
 
 def get_japanese_emoticon
